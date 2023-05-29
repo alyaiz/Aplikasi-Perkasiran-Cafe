bc9831214fb0c78933a94cf681a250c97345666e
alyaiz <alyaizzahzalfa@gmail.com>
2023-05-29 20:36:11 +0700

    mengubah link css

--- menuCof.php
+++ menuCof.php
@@ -9,7 +9,7 @@ include('koneksi1.php');
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="../css/menu.css" />
+  <link rel="stylesheet" href="css/menu.css" />
   <title>CAFE CASH</title>
 </head>
 
